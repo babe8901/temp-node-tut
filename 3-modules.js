7ab1d72b11bff09ca84d252045131480c1b9d4a3
Shubham Yadav <luckyshubham16@gmail.com>
2022-03-30 23:51:38 +0530

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,10 @@
+const { john, peter } = require('./4-names')
+const sayHi = require('./5-utils')
+
+const data = require('./6-alternative-flavour')
+
+const c = require('./7-mind-grenade')
+// console.log(c);
+sayHi("Susan")
+sayHi(john)
+sayHi(peter)
